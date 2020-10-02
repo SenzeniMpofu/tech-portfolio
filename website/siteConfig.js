a84934c3446f6c4a7cb73b3a5212bf45c129619d
SenzeniMpofu <37525541+SenzeniMpofu@users.noreply.github.com>
2020-10-02 12:16:16 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,8 +12,8 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Senzeni Mpofu', // Title for your website.
+    tagline: 'Code. Food. Dance',
     profilePhoto: 'img/profile.png',
     url: 'https://joatmon08.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
